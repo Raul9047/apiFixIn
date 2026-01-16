f7d339ddc06cbfd2b014c25d57350d4146d10c5a
raul9047 <raul_9047@hotmail.com>
2026-01-16 14:52:26 -0400

    Arreglado Firebase Private Key

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,26 +1,26 @@
-// config/firebase.js
 const admin = require('firebase-admin');
 require('dotenv').config();
 
-// --- INICIO DEL FIX PARA RENDER ---
 let privateKey = process.env.FIREBASE_PRIVATE_KEY;
 
 if (privateKey) {
   if (privateKey.startsWith('"') && privateKey.endsWith('"')) {
     privateKey = privateKey.slice(1, -1);
   }
-  
   privateKey = privateKey.replace(/\\n/g, '\n');
 }
 
 try {
-  const serviceAccount = require('./firebase-key.json'); 
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: privateKey,
+    }),
   });
-  console.log('Firebase Admin SDK inicializado.');
+  console.log("✅ Firebase Admin inicializado correctamente.");
 } catch (error) {
-  console.error('Error Firebase:', error.message);
+  console.error("❌ Error inicializando Firebase:", error);
 }
 
 module.exports = admin;
