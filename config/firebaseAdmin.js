c9f5db8726457e18d624f7fddcd3f75318f3c10f
raul9047 <raul_9047@hotmail.com>
2026-01-16 14:37:57 -0400

    Arreglado Firebase Private Key

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,5 +1,17 @@
 // config/firebase.js
 const admin = require('firebase-admin');
+require('dotenv').config();
+
+// --- INICIO DEL FIX PARA RENDER ---
+let privateKey = process.env.FIREBASE_PRIVATE_KEY;
+
+if (privateKey) {
+  if (privateKey.startsWith('"') && privateKey.endsWith('"')) {
+    privateKey = privateKey.slice(1, -1);
+  }
+  
+  privateKey = privateKey.replace(/\\n/g, '\n');
+}
 
 try {
   const serviceAccount = require('./firebase-key.json'); 
